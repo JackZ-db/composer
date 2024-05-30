4f2754c27768e8922a83663dd0e30994f2b68d67
Jack Zhang <170473087+JackZ-db@users.noreply.github.com>
2024-05-30 20:26:06 +0000

    refactored so that to string in valuewithrank matches our UI

--- composer/callbacks/global_straggler_detector.py
+++ composer/callbacks/global_straggler_detector.py
@@ -69,13 +69,14 @@ class _ValueWithRank:
         """
         return self._value, self._rank, self._unit
 
+    #edited __str__ to exclude rank, which is now printed with key
     def __str__(self) -> str:
         """String representation of the object
         Returns:
             str: strigified object
         """
 
-        return f"{self._value:.2f}{self._unit}/{self._rank}"
+        return f"{self._value:.2f}{self._unit}"
 
 
 @dataclass
@@ -430,20 +431,20 @@ class StragglerDetector:
 
 
                 min_max_data = {
-                    "MinRoundTripTime/Rank-" + str(o_dt.min_elapsed._rank): o_dt.min_elapsed._value + o_dt.min_elapsed._unit,
-                    "MaxRoundTripTime/Rank-" + str(o_dt.max_elapsed._rank): o_dt.max_elapsed._value + o_dt.max_elapsed._unit,
-                    "MinPower/Rank-" + str(o_dt.min_power._rank): o_dt.min_power._value + o_dt.min_power._unit,
-                    "MaxPower/Rank-" + str(o_dt.max_power._rank): o_dt.max_power._value + o_dt.max_power._unit,
-                    "MinTemp/Rank-" + str(o_dt.min_temp._rank): o_dt.min_temp._value + o_dt.min_temp._unit,
-                    "MaxTemp/Rank-" + str(o_dt.max_temp._rank): o_dt.max_temp._value + o_dt.max_temp._unit,
-                    "MinUtilization/Rank-" + str(o_dt.min_util._rank): o_dt.min_util._value + o_dt.min_util._unit,
-                    "MaxUtilization/Rank-" + str(o_dt.max_util._rank): o_dt.max_util._value + o_dt.max_util._unit,
-                    "MinClock/Rank-" + str(o_dt.min_clock._rank): o_dt.min_clock._value + o_dt.min_clock._unit,
-                    "MaxClock/Rank-" + str(o_dt.max_clock._rank): o_dt.max_clock._value + o_dt.max_clock._unit,
-                    "MinBatchLoadLatency/Rank-" + str(o_dt.min_btime._rank): o_dt.min_btime._value + o_dt.min_btime._unit,
-                    "MaxBatchLoadLatency/Rank-" + str(o_dt.max_btime._rank): o_dt.max_btime._value + o_dt.max_btime._unit,
-                    "MinThroughput/Rank-" + str(min_throughput._rank): min_throughput._value + min_throughput._unit,
-                    "MaxThroughput/Rank-" + str(max_throughput._rank): max_throughput._value + max_throughput._unit
+                    "MinRoundTripTime/Rank-" + str(o_dt.min_elapsed._rank): o_dt.min_elapsed,
+                    "MaxRoundTripTime/Rank-" + str(o_dt.max_elapsed._rank): o_dt.max_elapsed,
+                    "MinPower/Rank-" + str(o_dt.min_power._rank): o_dt.min_power,
+                    "MaxPower/Rank-" + str(o_dt.max_power._rank): o_dt.max_power,
+                    "MinTemp/Rank-" + str(o_dt.min_temp._rank): o_dt.min_temp,
+                    "MaxTemp/Rank-" + str(o_dt.max_temp._rank): o_dt.max_temp,
+                    "MinUtilization/Rank-" + str(o_dt.min_util._rank): o_dt.min_util,
+                    "MaxUtilization/Rank-" + str(o_dt.max_util._rank): o_dt.max_util,
+                    "MinClock/Rank-" + str(o_dt.min_clock._rank): o_dt.min_clock,
+                    "MaxClock/Rank-" + str(o_dt.max_clock._rank): o_dt.max_clock,
+                    "MinBatchLoadLatency/Rank-" + str(o_dt.min_btime._rank): o_dt.min_btime,
+                    "MaxBatchLoadLatency/Rank-" + str(o_dt.max_btime._rank): o_dt.max_btime,
+                    "MinThroughput/Rank-" + str(min_throughput._rank): min_throughput,
+                    "MaxThroughput/Rank-" + str(max_throughput._rank): max_throughput
                     }
 
                 if self.mmcnt > 1 and self.mmcnt < self.world:
