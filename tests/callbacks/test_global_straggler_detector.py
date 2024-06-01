92c61d57bc26819a2ec925585f9a68f3cd3ae9ce
JackZ-db <jack.zhang@databricks.com>
2024-05-31 22:32:50 -0700

    reorderring methods for debugging

--- tests/callbacks/test_global_straggler_detector.py
+++ tests/callbacks/test_global_straggler_detector.py
@@ -40,6 +40,53 @@ def _assert_leq_hundred(logged_values, unit):
         assert v <= 100.00
 
 
+@device('gpu')
+@pytest.mark.parametrize('flops_per_batch', [True])
+@pytest.mark.gpu
+@pytest.mark.world_size(2)
+def test_global_straggler_detector_all(device, flops_per_batch: bool):
+    dist.initialize_dist(get_device(device))
+
+    # Construct the callbacks
+    global_straggler_detector = GlobalStragglerDetector(log_all_data=True)
+    in_memory_logger = InMemoryLogger()  # track the logged metrics in the in_memory_logger
+
+    model = SimpleModel()
+    if flops_per_batch:
+        model.flops_per_batch = lambda batch: len(batch) * 100.0  # pyright: ignore[reportGeneralTypeIssues]
+    
+    # Construct the trainer and train
+    dataset = RandomClassificationDataset()
+    trainer = Trainer(
+        model=model,
+        callbacks=global_straggler_detector,
+        loggers=in_memory_logger,
+        train_dataloader=DataLoader(dataset=dataset, sampler=DistributedSampler(dataset=dataset)),
+        max_duration='1ep',
+    )
+    trainer.fit()
+
+    print("Length of logger.data: " + str(len(in_memory_logger.data)))
+    for key in in_memory_logger.data.keys():
+        print(key)
+
+    # only rank 0 logs
+    if dist.get_local_rank() == 0:
+        for rank in range(0, dist.get_world_size()):
+            _assert_no_negative_values(in_memory_logger.data['RoundTripTime/Rank-' + str(rank)], "ms")
+            _assert_no_negative_values(in_memory_logger.data['Power/Rank-' + str(rank)], "W")
+            _assert_no_negative_values(in_memory_logger.data['Temp/Rank-' + str(rank)], "C")
+            _assert_no_negative_values(in_memory_logger.data['Utilization/Rank-' + str(rank)], "%")
+            _assert_leq_hundred(in_memory_logger.data['Utilization/Rank-' + str(rank)], "%")
+            _assert_no_negative_values(in_memory_logger.data['Clock/Rank-' + str(rank)], "MHz")
+            _assert_no_negative_values(in_memory_logger.data['BatchLoadLatency/Rank-' + str(rank)], "us")
+            _assert_no_negative_values(in_memory_logger.data['Throughput/Rank-' + str(rank)], "TF")
+
+        
+        num_batches = int(trainer.state.timestamp.batch)
+        for metric, valueList in in_memory_logger.data.items():
+            assert len(valueList) == num_batches
+
 @pytest.mark.parametrize('flops_per_batch', [True])
 @pytest.mark.gpu
 @pytest.mark.world_size(2)
@@ -104,49 +151,3 @@ def test_global_straggler_detector_min_max(flops_per_batch: bool):
         assert len(in_memory_logger.data['MinThroughput/Rank']) == num_batches
         assert len(in_memory_logger.data['MaxThroughput/Rank']) == num_batches
 
-@device('gpu')
-@pytest.mark.parametrize('flops_per_batch', [True])
-@pytest.mark.gpu
-@pytest.mark.world_size(2)
-def test_global_straggler_detector_all(device, flops_per_batch: bool):
-    dist.initialize_dist(get_device(device))
-
-    # Construct the callbacks
-    global_straggler_detector = GlobalStragglerDetector(log_all_data=True)
-    in_memory_logger = InMemoryLogger()  # track the logged metrics in the in_memory_logger
-
-    model = SimpleModel()
-    if flops_per_batch:
-        model.flops_per_batch = lambda batch: len(batch) * 100.0  # pyright: ignore[reportGeneralTypeIssues]
-    
-    # Construct the trainer and train
-    dataset = RandomClassificationDataset()
-    trainer = Trainer(
-        model=model,
-        callbacks=global_straggler_detector,
-        loggers=in_memory_logger,
-        train_dataloader=DataLoader(dataset=dataset, sampler=DistributedSampler(dataset=dataset)),
-        max_duration='1ep',
-    )
-    trainer.fit()
-
-    print("Length of logger.data: " + str(len(in_memory_logger.data)))
-    for key in in_memory_logger.data.keys():
-        print(key)
-
-    # only rank 0 logs
-    if dist.get_local_rank() == 0:
-        for rank in range(0, dist.get_world_size()):
-            _assert_no_negative_values(in_memory_logger.data['RoundTripTime/Rank-' + str(rank)], "ms")
-            _assert_no_negative_values(in_memory_logger.data['Power/Rank-' + str(rank)], "W")
-            _assert_no_negative_values(in_memory_logger.data['Temp/Rank-' + str(rank)], "C")
-            _assert_no_negative_values(in_memory_logger.data['Utilization/Rank-' + str(rank)], "%")
-            _assert_leq_hundred(in_memory_logger.data['Utilization/Rank-' + str(rank)], "%")
-            _assert_no_negative_values(in_memory_logger.data['Clock/Rank-' + str(rank)], "MHz")
-            _assert_no_negative_values(in_memory_logger.data['BatchLoadLatency/Rank-' + str(rank)], "us")
-            _assert_no_negative_values(in_memory_logger.data['Throughput/Rank-' + str(rank)], "TF")
-
-        
-        num_batches = int(trainer.state.timestamp.batch)
-        for metric, valueList in in_memory_logger.data.items():
-            assert len(valueList) == num_batches
