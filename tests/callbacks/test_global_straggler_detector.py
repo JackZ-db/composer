312047bdfff84c31ea46d6472490acef7360f8e8
JackZ-db <jack.zhang@databricks.com>
2024-06-03 09:45:01 -0700

    cleaning up batch length assertion logic

--- tests/callbacks/test_global_straggler_detector.py
+++ tests/callbacks/test_global_straggler_detector.py
@@ -82,13 +82,14 @@ def test_global_straggler_detector_all(device, flops_per_batch: bool):
             _assert_no_negative_values(in_memory_logger.data['Throughput/Rank-' + str(rank)], "TF")
 
         
-        num_batches = int(trainer.state.timestamp.batch)
-        for metric, valueList in in_memory_logger.data.items():
-            print("Metric: ")
-            print(metric)
-            for entry in valueList:
-                print(entry)
-            assert len(valueList) == num_batches
+            assert len(in_memory_logger.data['RoundTripTime/Rank-' + str(rank)]) == num_batches
+            assert len(in_memory_logger.data['Power/Rank-' + str(rank)]) == num_batches
+            assert len(in_memory_logger.data['Temp/Rank-' + str(rank)]) == num_batches
+            assert len(in_memory_logger.data['Utilization/Rank-' + str(rank)]) == num_batches
+            assert len(in_memory_logger.data['Clock/Rank-' + str(rank)]) == num_batches
+            assert len(in_memory_logger.data['BatchLoadLatency/Rank-' + str(rank)]) == num_batches
+            assert len(in_memory_logger.data['Throughput/Rank-' + str(rank)]) == num_batches
+
 
 """
 @device('gpu')
