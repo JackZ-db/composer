aa0308d1619290a8522047d0526e3a955547a6a9
jack-zhang_data <jack.zhang@databricks.com>
2024-07-29 13:50:04 -0700

    , device='cpu'

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -3240,12 +3240,12 @@ class Trainer:
             if self.state.auto_microbatching:
                 # Check if any other rank hit an OOM
                 #found_cuda_oom_tensor = self.state.device.tensor_to_device(torch.tensor([0], dtype=torch.uint8))
-                found_cuda_oom_tensor = torch.tensor([0], dtype=torch.uint8).cpu()
+                found_cuda_oom_tensor = torch.tensor([0], dtype=torch.uint8, device='cpu')
                 dist.all_reduce(found_cuda_oom_tensor, reduce_operation='MAX')
                 found_cuda_oom = found_cuda_oom_tensor.item()
                 # Signal current rank is still in batch
                 #all_ranks_finished_tensor = self.state.device.tensor_to_device(torch.tensor([0], dtype=torch.uint8))
-                all_ranks_finished_tensor = torch.tensor([0], dtype=torch.uint8).cpu()
+                all_ranks_finished_tensor = torch.tensor([0], dtype=torch.uint8, device='cpu')
                 dist.all_reduce(all_ranks_finished_tensor, reduce_operation='MIN')
 
                 if found_cuda_oom == 1:
